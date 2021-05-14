9227b2a70c4cffa645b3273c8366955e99d52778
dili <599181142@qq.com>
2021-05-14 16:05:51 +0800

    masater commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");  System.out.println("hellohot-fix2");
+        System.out.println("2222222222222222222222222hellohot-master");
     }
 }
